4be72c9e6975992b96725701968ef19ff0346150
shogaaa <114602442+shogaaa@users.noreply.github.com>
2022-11-29 14:52:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-let activeButton = "Amazon.com";
+let json= [{
+    "id": "d3ff0c68892",
+    "name": "Amazon.com",
+    "email": "contact@amazon.com",
+    "boxes": "6.8,7.9,3.6,8.8,4.8,9.4"
+  },
+  {
+    "id": "ad8b78578",
+    "name": "American Express",
+    "email": "contact@americanexpress.com",
+    "boxes": "9.8"
+  },
+  {
+    "id": "e40b72",
+    "name": "Walmart",
+    "email": "contact@walmart.com",
+    "boxes": "3.3,3,3.2,1.6,5.4,2.1,2.7,4.6"
+  },
+  {
+    "id": "34554e30e6ac1a4",
+    "name": "Apple",
+    "email": "contact@apple.com",
+    "boxes": "9.6,3.5"
+  }]
+
 const pressbut = document.getElementsByTagName("button");
 const input = document.getElementById("boxes");
 
@@ -10,8 +34,8 @@ class dataItem {
         this.boxes = boxes;
     }
     addToScreenData () {
-        const h3Tag = document.getElementById("companyName");
-        const aTag = document.getElementById("companyEmail");
+        const h3Tag = document.getElementById("Name");
+        const aTag = document.getElementById("Email");
         const boxesinput = document.getElementById("boxes");
         const spanTag = document.getElementById("result");
 
@@ -30,14 +54,14 @@ class dataItem {
         return Math.ceil(result / 10);
     }
 }
-
+let activeButton = "Amazon.com";
 function setActive(button){
     activeButton = button.id;
     let htmlbut = document.getElementById(activeButton);
     for(i=0;i<pressbut.length; i++){
         pressbut[i].className = 'notActive';
-    };
-    htmlbut.className = "active";
+        htmlbut.className = "active";};
+    
 
     fetch("https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json")
     .then(responseObj => responseObj.json(responseObj))
@@ -60,6 +84,5 @@ input.addEventListener("keypress", (e) => {
         document.getElementById("result").innerText = Math.ceil(result / 10);
     }
 });
-
-
 window.onload(setActive(document.getElementById("Amazon.com")));
+
